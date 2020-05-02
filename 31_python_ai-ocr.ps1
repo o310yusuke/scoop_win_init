d7c951ce0ddf32af0565b93e79eeb806dedcd9b5
o310yusuke <yusuke310@gmail.com>
2020-05-02 14:13:36 +0900

    add to pyocr install command

--- 31_python_ai-ocr.ps1
+++ 31_python_ai-ocr.ps1
@@ -10,5 +10,14 @@ scoop install tesseract
 # tesseractの言語ファイルをインストール
 scoop install tesseract-languages
 
+# pyocrをインストール
+Write-Output "----------------------------------------"
+Write-Output "pyocrをインストールしましょう。そのためにまずPython仮想環境を準備しましょう。"
+Write-Output "    $ cd [project directory]" "    $ python -m venv [new venv name]"
+Write-Output "    ※[new venv name]は「venv」にしておくと、何のためのディレクトリか後で分かりやすい"
+Write-Output "仮想環境を作成後に下記コマンドを実行してpyocrをインストール"
+Write-Output "    $ python -m pip install pyocr"
+Write-Output "----------------------------------------"
+
 # VSCode拡張機能をインストール
 # code --install-extension [name]
