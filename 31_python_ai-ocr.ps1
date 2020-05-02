a1366a51dc18eebed44fa234bb18ddc271f3c6c4
o310yusuke <yusuke310@gmail.com>
2020-05-02 13:35:43 +0900

    add AI-OCR of tesseract

---
+++ 31_python_ai-ocr.ps1
@@ -0,0 +1,14 @@
+# ========================================
+# PythonでAI-OCRを利用するためのツールインストール
+# ----------------------------------------
+# 特記事項なし
+# ========================================
+
+# tesseractをインストール
+scoop install tesseract
+
+# tesseractの言語ファイルをインストール
+scoop install tesseract-languages
+
+# VSCode拡張機能をインストール
+# code --install-extension [name]
