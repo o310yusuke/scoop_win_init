acec71cf0a779fac779a5bc4f30f6f7f013337ac
o310yusuke <yusuke310@gmail.com>
2020-07-24 15:31:19 +0900

    create markdonw2pdf tools

---
+++ 11_markdown2pdf.ps1
@@ -0,0 +1,14 @@
+﻿# ========================================
+# Markdownからプレゼン資料（PDF）を作る
+# ----------------------------------------
+# 
+# ========================================
+
+# VSCode拡張機能をインストール
+## Marp
+code --install-extension marp-team.marp-vscode
+## Draw.io
+code --install-extension hediet.vscode-drawio
+## Graph Vega
+code --install-extension randomfractalsinc.vscode-vega-viewer
+
