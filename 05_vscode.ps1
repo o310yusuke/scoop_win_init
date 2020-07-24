300e2e09585a6fd17b8936230865deb6048081ae
o310yusuke <yusuke310@gmail.com>
2020-07-24 15:32:16 +0900

    開発時のログ調査やデータ分析に利用できそうか拡張機能を追加

--- 05_vscode.ps1
+++ 05_vscode.ps1
@@ -16,3 +16,14 @@ $Shortcut.Save()
 # VSCode拡張機能をインストール
 # code --install-extension [name]
 
+# カッコの開始と終了を自動で色分けし、開始と終了を線で繋いでくれる
+code --install-extension CoenraadS.bracket-pair-colorizer
+
+# csvデータを色付け
+# code --install-extension mechatroner.rainbow-csv
+
+# logデータを色付け
+# code --install-extension emilast.LogFileHighlighter
+
+# 関連付けられた外部アプリで開く
+# code --install-extension YuTengjing.open-in-external-app
